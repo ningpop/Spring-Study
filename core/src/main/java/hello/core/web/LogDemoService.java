0a91e9779ff71672e720329797e1910a5f5f2f58
ningpop <craft1933@naver.com>
2022-03-15 13:46:13 +0900

    [스프링 핵심 원리 - 기본편] 빈 스코프 (스코프와 프록시)

--- core/src/main/java/hello/core/web/LogDemoService.java
+++ core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
