bcc119abf36558913f79042de09c2557efd5f4f0
ningpop <craft1933@naver.com>
2022-03-29 20:14:29 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 영속성 관리 - 내부 동작 방식

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -38,14 +38,55 @@ public class JpaMain {
             // 저장 코드를 작성하지 않아도 됨.(update 쿼리를 만들어줌)
 
             // 전체 회원 조회 (페이징)
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(10)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
 
+            // 비영속
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+
+            // 영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+
+//            Member findMember = em.find(Member.class, 101L); // 1차 캐시에서 조회하기 때문에 select문 쿼리를 날리지 않음
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("===============");
+
+//            Member member = em.find(Member.class, 150L);
+//            member.setName("ZZZZZ");
+//            //em.persist(member); // 사용 X
+//            System.out.println("===============");
+
+//            Member member = new Member(200L, "member200");
+//            em.persist(member);
+//            em.flush();
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+            //em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
+
+            System.out.println("===============");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
