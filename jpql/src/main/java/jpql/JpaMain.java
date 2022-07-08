39a6cc3bf4dd3f20d070be43d23b832d1588952d
ningpop <craft1933@naver.com>
2022-07-08 13:58:35 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 객체지향 쿼리 언어2 - 중급 문법 (페치조인 ~ 다형성 쿼리)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -52,18 +52,28 @@ public class JpaMain {
 //                    .getSingleResult();
 //            System.out.println("singleResult = " + result.getUsername());
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
@@ -126,12 +136,31 @@ public class JpaMain {
 //            String query = "select concat('a', 'b') from Member m";
 //            String query = "select locate('de', 'abcdef') from Member m";
 //            String query = "select size(t.members) from Team t";
-            String query = "select t.members from Team t";
-            Collection result = em.createQuery(query, Collection.class)
+//            String query = "select t.members from Team t";
+//            String query = "select m from Member m join fetch m.team";
+//            String query = "select t from Team t join fetch t.members";
+            String query = "select t from Team t";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
-            System.out.println("result = " + result);
+            System.out.println("result = " + result.size());
 
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//                //회원1, 팀A(SQL)
+//                //회원2, 팀A(1차캐시)
+//                //회원3, 팀B(SQL)
+//
+//                //그럼 회원 100명이라면? 100번의 쿼리. -> "N + 1 문제" 발생 -> 페치 조인으로 지연 로딩을 없애서 해결
+//            }
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|Members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+            }
 
             tx.commit();
         } catch (Exception e) {
--- jpql/src/main/java/jpql/Team.java
+++ jpql/src/main/java/jpql/Team.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,6 +17,7 @@ public class Team {
 
     private String name;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
