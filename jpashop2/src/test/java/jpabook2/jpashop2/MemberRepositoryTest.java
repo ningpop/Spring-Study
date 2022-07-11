601670176ec02518400a45a28cc3e0add084ce4e
ningpop <craft1933@naver.com>
2022-07-11 16:50:16 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 프로젝트 환경설정 (JPA와 DB 설정, 동작확인)

---
+++ jpashop2/src/test/java/jpabook2/jpashop2/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook2.jpashop2;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false) // Transaction을 날린 후 자동으로 롤백하는 기능을 해제
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findMember == member: " + (findMember == member));
+
+    }
+}
