a8cc0bf303565f5c2e6cdd8ae191fd9e6996ab4f
ningpop <craft1933@naver.com>
2022-07-13 14:28:35 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 회원 도메인 개발 (회원 서비스 개발)

--- jpashop2/src/main/java/jpabook2/jpashop2/repository/MemberRepository.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook2.jpashop2.repository;
 
 import jpabook2.jpashop2.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,11 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
     @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
