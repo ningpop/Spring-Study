5df2a812183d7b3d7b7313c97a73fbd76872a52d
ningpop <craft1933@naver.com>
2022-07-14 15:43:33 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 상품 도메인 개발 (상품 엔티티 개발(비즈니스 로직 추가))

--- jpashop2/src/main/java/jpabook2/jpashop2/domain/item/Item.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook2.jpashop2.domain.item;
 
 import jpabook2.jpashop2.domain.Category;
+import jpabook2.jpashop2.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,11 +20,28 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // == 비즈니스 로직 ==
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ jpashop2/src/main/java/jpabook2/jpashop2/exception/NotEnoughStockException.java
@@ -0,0 +1,21 @@
+package jpabook2.jpashop2.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
