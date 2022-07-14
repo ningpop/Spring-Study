7ae06524d749caed1db1dea33aecd172536357c0
ningpop <craft1933@naver.com>
2022-07-14 18:15:36 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 주문 도메인 개발 (주문 리포지토리 개발)

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook2.jpashop2.repository;
+
+import jpabook2.jpashop2.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+}
