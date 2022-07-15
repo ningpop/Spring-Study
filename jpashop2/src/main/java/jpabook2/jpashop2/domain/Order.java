c2c17291b857fa45a644c257b13ff17d7e01b38a
ningpop <craft1933@naver.com>
2022-07-15 11:27:07 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 주문 도메인 개발 (주문 서비스 개발)

--- jpashop2/src/main/java/jpabook2/jpashop2/domain/Order.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook2.jpashop2.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id @GeneratedValue
--- jpashop2/src/main/java/jpabook2/jpashop2/domain/OrderItem.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook2.jpashop2.domain;
 
 import jpabook2.jpashop2.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
