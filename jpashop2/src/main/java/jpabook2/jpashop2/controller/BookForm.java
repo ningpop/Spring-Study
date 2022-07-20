b388f247aff9c06724d0f0add47d9b94c2aa69cd
ningpop <craft1933@naver.com>
2022-07-20 15:41:21 +0900

    [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 웹 계층 개발 (상품 등록)

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/controller/BookForm.java
@@ -0,0 +1,17 @@
+package jpabook2.jpashop2.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
