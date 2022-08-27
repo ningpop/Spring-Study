b1be2ca05f89d27eca338805cc908c6ed094574d
ningpop <craft1933@naver.com>
2022-08-27 20:54:48 +0900

    [실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화] 회원 수정 API

--- jpashop2/src/main/java/jpabook2/jpashop2/api/MemberApiController.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/api/MemberApiController.java
@@ -2,12 +2,10 @@ package jpabook2.jpashop2.api;
 
 import jpabook2.jpashop2.domain.Member;
 import jpabook2.jpashop2.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -36,6 +34,28 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
     @Data
--- jpashop2/src/main/java/jpabook2/jpashop2/service/MemberService.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/service/MemberService.java
@@ -43,4 +43,10 @@ public class MemberService {
     public Member findOne(Long member_id) {
         return memberRepository.findOne(member_id);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
