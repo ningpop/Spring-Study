c6185fc12ddca64d89f092ca233663f80faddf6d
ningpop <craft1933@naver.com>
2022-08-27 20:40:44 +0900

    [실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화] 회원 등록 API

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/api/MemberApiController.java
@@ -0,0 +1,56 @@
+package jpabook2.jpashop2.api;
+
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
+@RestController // @Controller + @ResponseBody = @RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /**
+     * v1처럼 엔티티를 직접 넣어주는 것이 아니라,
+     * DTO를 만들어서 넣어줄 것.
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        @NotEmpty // 엔티티가 아니라 여기에서 validation을 잡을 것.
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+}
--- jpashop2/src/main/java/jpabook2/jpashop2/domain/Member.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty // 필수값 지정
     private String name;
 
     @Embedded
